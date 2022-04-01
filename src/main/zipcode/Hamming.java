408fbeab0b9b13577ceff786b25ad95d02570256
meredith <meredithbrown302@gmail.com>
2022-04-01 14:45:53 -0400

    lab complete

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,10 +1,24 @@
 package zipcode;
 
 public class Hamming {
+    String s;
+    String s1;
+    Integer count = 0;
+
     public Hamming(String s, String s1) {
+        if (s.length() != s1.length()) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
+        this.s = s;
+        this.s1 = s1;
     }
 
     public int getHammingDistance() {
-        return -1;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != s1.charAt(i)) {
+                count++;
+            }
+        }
+        return count;
     }
 }
